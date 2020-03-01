7bd5213c07c81988a68ae763d10bc88a41b18ab7
sandhuravneet007 <ravneet.sandhu@bizongo.com>
2020-03-02 02:10:26 +0530

    crud blog app

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://95d8138a.ngrok.io'
+})
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context as BlogContext} from "../context/BlogContext";
 import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = ({ navigation }) => {
-    const {state, addBlogPost, deleteBlogPost} = useContext(BlogContext);
+    const {state, deleteBlogPost, getBlogPosts} = useContext(BlogContext);
+
+    useEffect(() => {
+        getBlogPosts();
+
+        const listener = navigation.addListener('didFocus', () => {
+            getBlogPosts();
+        });
+
+        return () => {
+            listener.remove();
+        };
+    }, []);
 
     return (
       <View>
